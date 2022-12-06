097c62f254bf6121342266e29c6d276460b850f1
Jeff Mahoney <jeffm@suse.com>
2022-12-05 20:22:51 -0500

    Add helpers to access tags in maps

--- pkg/rpmtags.go
+++ pkg/rpmtags.go
@@ -3,6 +3,8 @@ package rpmdb
 
 import (
 	"fmt"
+	"log"
+	"time"
 )
 
 const (
@@ -85,4 +87,244 @@ func typeName(typeid int32) string {
 	}
 
 	return typename
+}
+
+func (pkg *PackageInfoMap) GetStringTag(tagid int32) (string, error) {
+	tag, ok := pkg.Tags[tagid]
+	if !ok {
+		return "", fmt.Errorf("Package has no tag \"%s\"", tagName(tagid))
+	}
+
+	value, ok := tag.Value.(string)
+	if !ok {
+		return "", fmt.Errorf("Expected string value for tag \"%s\"", tagName(tagid))
+	}
+
+	return value, nil
+}
+
+func (pkg *PackageInfoMap) GetStringArrayTag(tagid int32) ([]string, error) {
+	tag, ok := pkg.Tags[tagid]
+	if !ok {
+		return nil, fmt.Errorf("Package has no tag \"%s\"", tagName(tagid))
+	}
+
+	value, ok := tag.Value.([]string)
+	if !ok {
+		return nil, fmt.Errorf("Expected string value for tag \"%s\"", tagName(tagid))
+	}
+
+	return value, nil
+}
+
+func (pkg *PackageInfoMap) GetIntTag(tagid int32) (int, error) {
+	tag, ok := pkg.Tags[tagid]
+	if !ok {
+		return 0, fmt.Errorf("Package has no tag \"%s\"", tagName(tagid))
+	}
+
+	switch v := tag.Value.(type) {
+	case uint16:
+		return int(v), nil
+	case int32:
+		return int(v), nil
+	case uint32:
+		return int(v), nil
+	case int64:
+		return int(v), nil
+	case uint64:
+		return int(v), nil
+	default:
+		return 0, fmt.Errorf("Expected integer value for tag \"%s\"", tagName(tagid))
+	}
+}
+
+func (pkg *PackageInfoMap) GetIntArrayTag(tagid int32) ([]int, error) {
+	tag, ok := pkg.Tags[tagid]
+	if !ok {
+		return nil, fmt.Errorf("Package has no tag \"%s\"", tagName(tagid))
+	}
+
+
+	switch v := tag.Value.(type) {
+	case []uint16:
+		ret := make([]int, len(v))
+		for i, val := range v {
+			ret[i] = int(val)
+		}
+
+		return ret, nil
+	case []int32:
+		ret := make([]int, len(v))
+		for i, val := range v {
+			ret[i] = int(val)
+		}
+
+		return ret, nil
+	case []uint32:
+		ret := make([]int, len(v))
+		for i, val := range v {
+			ret[i] = int(val)
+		}
+
+		return ret, nil
+	case []int64:
+		ret := make([]int, len(v))
+		for i, val := range v {
+			ret[i] = int(val)
+		}
+
+		return ret, nil
+	case []uint64:
+		ret := make([]int, len(v))
+		for i, val := range v {
+			ret[i] = int(val)
+		}
+
+		return ret, nil
+	default:
+		return nil, fmt.Errorf("Expected integer array value for tag \"%s\"", tagName(tagid))
+	}
+}
+
+func (pkg *PackageInfoMap) GetTimeTag(tagid int32) (time.Time, error) {
+	tag, ok := pkg.Tags[tagid]
+	if !ok {
+		return time.Time{}, fmt.Errorf("Package has no tag \"%s\"", tagName(tagid))
+	}
+
+	value, ok := tag.Value.(time.Time)
+	if !ok {
+		return time.Time{}, fmt.Errorf("Expected time value for tag \"%s\"", tagName(tagid))
+	}
+
+	return value, nil
+}
+
+
+func (pkg *PackageInfoMap) Name() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_NAME))
+}
+
+func (pkg *PackageInfoMap) ModularityLabel() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_MODULARITYLABEL))
+}
+
+func (pkg *PackageInfoMap) Version() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_VERSION))
+}
+
+func (pkg *PackageInfoMap) Release() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_RELEASE))
+}
+
+func (pkg *PackageInfoMap) Arch() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_ARCH))
+}
+
+func (pkg *PackageInfoMap) SourceRPM() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_SOURCERPM))
+}
+
+func (pkg *PackageInfoMap) License() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_LICENSE))
+}
+
+func (pkg *PackageInfoMap) Vendor() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_VENDOR))
+}
+
+func (pkg *PackageInfoMap) Distribution() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_DISTRIBUTION))
+}
+
+func (pkg *PackageInfoMap) URL() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_URL))
+}
+
+func (pkg *PackageInfoMap) Summary() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_SUMMARY))
+}
+
+func (pkg *PackageInfoMap) Description() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_DESCRIPTION))
+}
+
+func (pkg *PackageInfoMap) Group() string {
+	return AssumeValid(pkg.GetStringTag(RPMTAG_GROUP))
+}
+
+func (pkg *PackageInfoMap) DirNames() []string {
+	return AssumeValid(pkg.GetStringArrayTag(RPMTAG_DIRNAMES))
+}
+
+func (pkg *PackageInfoMap) BaseNames() []string {
+	return AssumeValid(pkg.GetStringArrayTag(RPMTAG_BASENAMES))
+}
+
+func (pkg *PackageInfoMap) FileDigests() []string {
+	return AssumeValid(pkg.GetStringArrayTag(RPMTAG_FILEDIGESTS))
+}
+
+func (pkg *PackageInfoMap) FileUserName() []string {
+	return AssumeValid(pkg.GetStringArrayTag(RPMTAG_FILEUSERNAME))
+}
+
+func (pkg *PackageInfoMap) FileGroupName() []string {
+	return AssumeValid(pkg.GetStringArrayTag(RPMTAG_FILEGROUPNAME))
+}
+
+func (pkg *PackageInfoMap) FileModes() []int {
+	return AssumeValid(pkg.GetIntArrayTag(RPMTAG_FILEMODES))
+}
+
+func (pkg *PackageInfoMap) Size() int {
+	return AssumeValid(pkg.GetIntTag(RPMTAG_SIZE))
+}
+
+func (pkg *PackageInfoMap) DirIndexes() []int {
+	return AssumeValid(pkg.GetIntArrayTag(RPMTAG_DIRINDEXES))
+}
+
+func (pkg *PackageInfoMap) FileSizes() []int {
+	return AssumeValid(pkg.GetIntArrayTag(RPMTAG_FILESIZES))
+}
+
+func (pkg *PackageInfoMap) FileFlags() []int {
+	return AssumeValid(pkg.GetIntArrayTag(RPMTAG_FILEFLAGS))
+}
+
+func (pkg *PackageInfoMap) InstallTime() time.Time {
+	return AssumeValid(pkg.GetTimeTag(RPMTAG_INSTALLTIME))
+}
+
+func (pkg *PackageInfoMap) BuildTime() time.Time {
+	return AssumeValid(pkg.GetTimeTag(RPMTAG_BUILDTIME))
+}
+
+func (pkg *PackageInfoMap) Epoch() int {
+	return AssumeValid(pkg.GetIntTag(RPMTAG_EPOCH))
+}
+
+func (pkg *PackageInfoMap) PGP() (*PGPInfo, error) {
+	tagid := int32(RPMTAG_PGP)
+
+	tag, ok := pkg.Tags[tagid]
+	if !ok {
+		return nil, fmt.Errorf("Package has no tag \"%s\"", tagName(tagid))
+	}
+
+	value, ok := tag.Value.(*PGPInfo)
+	if !ok {
+		return nil, fmt.Errorf("Expected *PGPInfo value for tag \"%s\"", tagName(tagid))
+	}
+
+	return value, nil
+}
+
+func AssumeValid[T any](first T, err error) T {
+	if err != nil {
+		log.Fatal(err)
+	}
+	return first
 }
